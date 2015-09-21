0531a8cd51ab08c8e6ef6772d083b3b56855ff12
Sean Dixon <smd686s@gmail.com>
2015-09-21 14:25:40 -0500

    Revert engine to test loading....

--- lib/react/rails/engine.rb
+++ lib/react/rails/engine.rb
@@ -2,8 +2,9 @@ module React
   module Rails
     class Engine < ::Rails::Engine
       initializer "react_rails.setup_engine", :group => :all do |app|
-        sprockets_env = app.assets || Sprockets # Sprockets 3.x expects this in a different place
-        sprockets_env.register_engine(".jsx", React::JSX::Template)
+        app.assets.register_engine '.jsx', React::JSX::Template
+        # sprockets_env = app.assets || Sprockets # Sprockets 3.x expects this in a different place
+        # sprockets_env.register_engine(".jsx", React::JSX::Templat`e)
       end
     end
   end
